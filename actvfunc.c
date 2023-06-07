6110bb2a9ab6347c9c34320d1d92fcabaabf6d8d
xq100shx <krzysiek.dawidczyk03@gmail.com>
2023-06-07 23:50:12 +0200

    Added some actv functions (all checked, mb it'll be fine)

--- actvfunc.c
+++ actvfunc.c
@@ -1,5 +1,29 @@
 //
 // Created by Krzysiek on 07.06.2023.
 //
-
+#include <math.h>
 #include "actvfunc.h"
+
+float sigmoid(float x){
+    return 1.0f/(1.0f+expf(-x));
+}
+float d_sigmoid(float x){
+    return sigmoid(x)*(1- sigmoid(x));
+}
+float ReLU(float x){    //im not sure if it's correct
+    if(x>0) return x;
+    else return 0.0f;
+}
+float d_ReLU(float x){  //im not sure if it's correct
+    if(x>0) return 1.0f;
+    else return 0.0f;
+}
+float Leaky_ReLu(float x){  //im not sure if it's correct
+    if(x>0) return x;
+    else return 0.01f*x;
+}
+float d_Leaky_ReLu(float x){    //im not sure if it's correct
+    if(x>0) return 1.0f;
+    else return 0.01f;
+}
+//note add matrix interpretations of functions to matrix lib
--- actvfunc.h
+++ actvfunc.h
@@ -4,7 +4,12 @@
 
 #ifndef NEURALNETWORK_ACTVFUNC_H
 #define NEURALNETWORK_ACTVFUNC_H
+
 float sigmoid(float x);
 float d_sigmoid(float x);
+float ReLU(float x);
+float d_ReLU(float x);
+float Leaky_ReLu(float x);
+float d_Leaky_ReLu(float x);
 
 #endif //NEURALNETWORK_ACTVFUNC_H
