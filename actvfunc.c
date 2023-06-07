116f693f4f372022412c30e9512f0bd086718b77
xq100shx <krzysiek.dawidczyk03@gmail.com>
2023-06-07 22:01:58 +0200

    Started working on activation functions library

---
+++ actvfunc.c
@@ -0,0 +1,5 @@
+//
+// Created by Krzysiek on 07.06.2023.
+//
+
+#include "actvfunc.h"
---
+++ actvfunc.h
@@ -0,0 +1,10 @@
+//
+// Created by Krzysiek on 07.06.2023.
+//
+
+#ifndef NEURALNETWORK_ACTVFUNC_H
+#define NEURALNETWORK_ACTVFUNC_H
+float sigmoid(float x);
+float d_sigmoid(float x);
+
+#endif //NEURALNETWORK_ACTVFUNC_H
